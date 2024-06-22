de547b3f55ee6bb053e08ef5a54ae9d76a4f8f25
NEEL PIYUSHKUMAR PATEL <nl914739@dal.ca>
2024-06-22 11:18:44 +0530

    Authentication feature

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,10 +2,53 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_health_core/styles/app_colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 // LoginPage provides a simple and secure user login interface.
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  Future<void> _login() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      print('Email and password cannot be empty');
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      print('Login successful');
+      Navigator.pushReplacementNamed(context, '/home');
+    } on FirebaseAuthException catch (e) {
+      print('Failed to login: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to login: ${e.message}')),
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,8 +80,9 @@ class LoginPage extends StatelessWidget {
                 ),
                 Spacer(),
                 TextField(
+                  controller: _emailController,
                   decoration: InputDecoration(
-                    hintText: 'Username',
+                    hintText: 'Email',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(
                         Radius.circular(12),
@@ -52,6 +96,8 @@ class LoginPage extends StatelessWidget {
                   height: 16,
                 ),
                 TextField(
+                  controller: _passwordController,
+                  obscureText: true,
                   decoration: InputDecoration(
                     hintText: 'Password',
                     border: OutlineInputBorder(
@@ -82,14 +128,17 @@ class LoginPage extends StatelessWidget {
                   height: 48,
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    },
+                    onPressed: _isLoading ? null : _login,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: AppColors.backgroundGreen,
                       foregroundColor: AppColors.white,
                     ),
-                    child: Text('Login'),
+                    child: _isLoading
+                        ? CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(AppColors.white),
+                          )
+                        : Text('Login'),
                   ),
                 ),
                 SizedBox(
